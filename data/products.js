e06357ce55f83ca84bdf3bbf0e3f4d9265b71e39
Shohan <hsohan509@gmail.com>
2026-05-23 01:36:04 +0600

    Create atomated test for Product classes

--- data/products.js
+++ data/products.js
@@ -10,7 +10,7 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
-class Product {
+export class Product {
   id;
   image;
   name;
@@ -40,7 +40,7 @@ class Product {
   }
 }
 
-class Clothing extends Product {
+export class Clothing extends Product {
   sizeChartLink;
   constructor(prodctDetails) {
     super(prodctDetails);
@@ -52,7 +52,7 @@ class Clothing extends Product {
   }
 }
 
-class Appliance extends Product {
+export class Appliance extends Product {
   instructionsLink;
   warrantyLink;
   constructor(prodctDetails) {
